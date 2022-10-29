2b02e6a3c7c04e938e10b0aa75f102d8fce60beb
EC2 Default User <ec2-user@ip-172-31-91-28.ec2.internal>
2022-10-29 15:22:16 +0000

    added locals.tf

---
+++ alb_v1.1/variables.tf
@@ -0,0 +1,41 @@
+variable "env" {
+  type        = string
+  description = "The environment"
+  default     = "dev"
+}
+variable "app_port" {
+  type        = number
+  description = "The port number of the application"
+  default     = "80"
+}
+variable "vpc_id" {
+  type        = string
+  description = "The id of the vpc id"
+  default     = "vpc-081185838a84ff65b"
+}
+
+variable "subnets" {
+  type = list(string)
+  description = "list of the subnets"
+  default = ["subnet-04b20fb6914d08786","subnet-0695ed0667a520d55","subnet-070faf8706c6839b6"]
+}
+
+variable "stage" {
+  type        = string
+  description = "the stage"
+  default     = "nonprod"
+}
+
+variable "project" {
+  type        = string
+  description = "the project"
+  default     = "cat"
+}
+
+
+variable "region" {
+  type        = string
+  description = "the region"
+  default     = "us-east-1"
+
+}
